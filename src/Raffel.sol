e6c6654ccfd16004cf13cb2f82de6ec0399ffc96
etabebe1 <etabebe1@gmail.com>
2024-11-19 04:16:59 -0500

    Chainlink keep/automation added to Raffel contract

--- src/Raffel.sol
+++ src/Raffel.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.24;
 
-import {VRFConsumerBaseV2Plus} from "../lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
-import {VRFV2PlusClient} from "../lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
 /**
  * @title A raffel contract
@@ -13,21 +13,50 @@ import {VRFV2PlusClient} from "../lib/chainlink-brownie-contracts/contracts/src/
 
 contract Raffel is VRFConsumerBaseV2Plus {
     error Raffel__SendMoreEth();
+    error Raffel__TransferFailed();
+    error Raffel_RaffelNotOpen();
+    /** Type declaration */
+    enum RaffelStatus {
+        OPEN,
+        CALCULATING
+    }
 
-    address payable[] private s_players;
-    uint256 private s_lastTimeStamp;
+    /** State Variables */
+    uint32 private constant NUM_WORDS = 1;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+
     uint256 private immutable i_entranceFee;
     uint256 private immutable i_interval;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
 
-    event RaffelEntered(address);
+    uint256 private s_lastTimeStamp;
+    address payable[] private s_players;
+    address payable private s_recentWinner;
+    RaffelStatus private s_raffelStatus;
+
+    event RaffelEntered(address indexed player);
+    event RaffelWinner(address indexed);
 
     // functions
     constructor(
-        uint enteranceFee,
-        address _vrfCoordinator
+        uint256 enteranceFee,
+        uint256 interval,
+        address _vrfCoordinator,
+        bytes32 _keyHash,
+        uint256 _subscriptionId,
+        uint32 _callbackGasLimit
     ) VRFConsumerBaseV2Plus(_vrfCoordinator) {
         i_entranceFee == enteranceFee;
+        i_interval = interval;
         s_lastTimeStamp == block.timestamp;
+
+        s_vrfCoordinator.requestRandomWords;
+        i_keyHash = _keyHash;
+        i_subscriptionId = _subscriptionId;
+        i_callbackGasLimit = _callbackGasLimit;
+        s_raffelStatus = RaffelStatus.OPEN;
     }
 
     function enterRaffel() external payable {
@@ -35,22 +64,85 @@ contract Raffel is VRFConsumerBaseV2Plus {
             revert Raffel__SendMoreEth();
         }
 
+        if (s_raffelStatus != RaffelStatus.OPEN) {
+            revert Raffel_RaffelNotOpen();
+        }
+
         s_players.push(payable(msg.sender));
         emit RaffelEntered(msg.sender);
     }
 
-    function pickWinner() external view {
-        if (s_lastTimeStamp - block.timestamp > i_interval) {
-            revert();
-        }
-        // vrf logic here
+    /**
+     * @dev This is the function that the Chainlink Keeper nodes call
+     * they look for `upkeepNeeded` to return True.
+     * the following should be true for this to return true:
+     * 1. The time interval has passed between raffle runs.
+     * 2. The lottery is open.
+     * 3. The contract has ETH.
+     * 4. Implicity, your subscription is funded with LINK.
+     */
+    function checkUpkeep(
+        bytes memory /* checkData */
+    ) public view returns (bool upkeepNeeded, bytes memory /* performData */) {
+        bool timeHasPassed = (s_lastTimeStamp - block.timestamp) >= i_interval;
+        bool raffelIsOpen = s_raffelStatus == RaffelStatus.OPEN;
+        bool raffelHasEth = address(this).balance > 0;
+        bool raffelHasPlayers = s_players.length > 0;
+
+        upkeepNeeded = (timeHasPassed &&
+            raffelIsOpen &&
+            raffelHasEth &&
+            raffelHasPlayers);
+
+        return (upkeepNeeded, "");
     }
 
+    function pickWinner() external {
+        if (s_lastTimeStamp - block.timestamp < i_interval) {
+            revert();
+        }
+
+        s_raffelStatus == RaffelStatus.CALCULATING;
+
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient
+            .RandomWordsRequest({
+                keyHash: i_keyHash,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATIONS,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUM_WORDS,
+                // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            });
+
+        uint256 requstId = s_vrfCoordinator.requestRandomWords(request);
+    }
+
+    // CEI: Check, Effect, Interaction
     // fulfillRandomWords function
     function fulfillRandomWords(
         uint256 requestId,
         uint256[] calldata randomWords
-    ) internal override {}
+    ) internal override {
+        // Check
+
+        // Effects
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        s_raffelStatus = RaffelStatus.OPEN;
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+        emit RaffelWinner(s_recentWinner);
+
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+
+        if (!success) {
+            revert Raffel__TransferFailed();
+        }
+    }
 
     // getter function
     function getEntranceFee() external view returns (uint256) {
