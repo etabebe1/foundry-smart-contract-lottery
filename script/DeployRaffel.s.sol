80c47e66ae8a03f1083e74e57cffb8c873de3628
etabebe1 <etabebe1@gmail.com>
2024-11-20 02:57:07 -0500

    NetworkConfig added for Mainnet, Sepolia and Anvil localchain in HelperConfig file, DeployRaffel updated to deploy Raffel contract along HelperConfig.sol

--- script/DeployRaffel.s.sol
+++ script/DeployRaffel.s.sol
@@ -1,4 +1,37 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.24;
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+import {Raffel} from "../src/Raffel.sol";
+import {HelperConfig} from "./HelperConfig.sol";
 
-contract DeployRaffel {}
+contract DeployRaffel is Script {
+    function deployContract() public returns (Raffel, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory networkConfig = helperConfig
+            .getConfig();
+
+        vm.startBroadcast();
+        Raffel raffel = new Raffel(
+            networkConfig.entranceFee,
+            networkConfig.interval,
+            networkConfig.vrfCoordinator,
+            networkConfig.keyHash,
+            networkConfig.subscriptionId,
+            networkConfig.callbackGasLimit
+        );
+        vm.stopBroadcast();
+
+        console.log("Raffel contract deployed at:", address(raffel));
+        console.log(
+            "HelperConfig contract deployed at:",
+            address(helperConfig)
+        );
+
+        return (raffel, helperConfig);
+    }
+
+    function run() public {
+        deployContract();
+    }
+}
---
+++ script/HelperConfig.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
+
+import {Script, console} from "forge-std/Script.sol";
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+
+abstract contract HelperConfigConstants {
+    uint256 ETH_MAINNET_CHAIN_ID = 1;
+    uint256 ETH_SEPOLIA_CHAIN_ID = 1115511;
+    uint256 ETH_ANVIL_CHAIN_ID = 31337;
+
+    /* VRF Mock values*/
+    uint96 baseFee = 0.25 ether;
+    uint96 gasPrice = 1e9;
+    int256 weiPerUintLink = 1e18;
+}
+
+contract HelperConfig is Script, HelperConfigConstants {
+    error HelperConfig__InvalidChainId();
+
+    struct NetworkConfig {
+        uint256 entranceFee;
+        uint256 interval;
+        address vrfCoordinator;
+        bytes32 keyHash;
+        uint256 subscriptionId;
+        uint32 callbackGasLimit;
+    }
+
+    NetworkConfig public activeNetworkConfig;
+    mapping(uint256 chainId => NetworkConfig) networkConfigs;
+
+    constructor() {
+        initializeNetworkConfig();
+        handleAnvilMock();
+        setActiveNetworkConfig();
+    }
+
+    function initializeNetworkConfig() internal {
+        // Mainnet NetworkConfig
+        networkConfigs[ETH_MAINNET_CHAIN_ID] = NetworkConfig({
+            entranceFee: 0.01 ether,
+            interval: 30,
+            vrfCoordinator: 0xD7f86b4b8Cae7D942340FF628F82735b7a20893a,
+            keyHash: 0x3fd2fec10d06ee8f65e7f2e95f5c56511359ece3f33960ad8a866ae24a8ff10b,
+            subscriptionId: 0,
+            callbackGasLimit: 50000
+        });
+
+        // Sepolia NetworkConfig
+        networkConfigs[ETH_SEPOLIA_CHAIN_ID] = NetworkConfig({
+            entranceFee: 0.01 ether,
+            interval: 30,
+            vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
+            keyHash: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+            subscriptionId: 0,
+            callbackGasLimit: 50000
+        });
+
+        //Anvil NetworkConfig
+        networkConfigs[ETH_ANVIL_CHAIN_ID] = NetworkConfig({
+            entranceFee: 0.01 ether,
+            interval: 30,
+            vrfCoordinator: address(0),
+            keyHash: bytes32(0),
+            subscriptionId: 0, //FIXME: SubId should be fixed form chain link SubId
+            callbackGasLimit: 50000
+        });
+    }
+
+    function setActiveNetworkConfig() internal returns (NetworkConfig memory) {
+        uint256 currentChainId = block.chainid;
+
+        if (networkConfigs[currentChainId].vrfCoordinator != address(0)) {
+            activeNetworkConfig = networkConfigs[currentChainId];
+
+            return activeNetworkConfig;
+        } else {
+            revert HelperConfig__InvalidChainId();
+        }
+    }
+
+    function handleAnvilMock() internal {
+        console.log("Deploying mock VRFCoordinator for Anvil ...");
+
+        VRFCoordinatorV2_5Mock vrfCoordinatorMockAddr = new VRFCoordinatorV2_5Mock(
+                baseFee,
+                gasPrice,
+                weiPerUintLink
+            );
+
+        networkConfigs[ETH_ANVIL_CHAIN_ID].vrfCoordinator = address(
+            vrfCoordinatorMockAddr
+        );
+
+        console.log("Mock deployed at:", address(vrfCoordinatorMockAddr));
+    }
+
+    function getConfig() external view returns (NetworkConfig memory) {
+        return networkConfigs[block.chainid];
+    }
+}
